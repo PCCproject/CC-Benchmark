f051b423c5810ff6e73bb72db472e7eaad113b8c
Nathan Jay <njay2@illinois.edu>
2018-10-24 15:56:01 -0500

    Moved additional functionality to utility scripts

--- python_utils/test_utils.py
+++ python_utils/test_utils.py
@@ -2,9 +2,39 @@ from python_utils import file_locations
 import json
 import os
 
+##
+#  Takes a topology name and reads the corresponding topology json into a dict.
+##
 def read_topology_to_dict(topology_name):
     topo_file = os.path.join(file_locations.topos_dir, topology_name + ".json")
     topo_json = None
     with open(topo_file) as f:
         topo_json = json.load(f)
     return topo_json
+
+##
+#   Reads a test or test list file and returns a list of filenames from which tests should be
+#   loaded.
+##
+def read_test_list_to_list(test_list):
+    test_file = os.path.join(file_locations.tests_dir, test_list) + ".json"
+    test_json = None
+    with open(test_file) as f:
+        test_json = json.load(f)
+
+    # If the filename passed in was already just a single test, then return a list with it's name
+    if not ("Tests" in test_json.keys()):
+        return [test_list]
+
+    # The filename passed in is for a test list, so return the list of tests
+    return test_json["Tests"]
+
+##
+#   Takes a test name and reads the corresponding test json into a dict.
+##
+def read_test_to_dict(test_name):
+    test_file = os.path.join(file_locations.tests_dir, test_name) + ".json"
+    test_json = None
+    with open(test_file) as f:
+        test_json = json.load(f)
+    return test_json
--- run_test.py
+++ run_test.py
@@ -2,43 +2,31 @@
 import os
 from setup import pantheon_setup
 from graphing.graph_utils import convert_pantheon_log
-from python_utils.test_utils import read_topology_to_dict
+from python_utils import file_locations
+from python_utils.test_utils import read_topology_to_dict, read_test_list_to_list, read_test_to_dict
 import subprocess
 import sys
 import time
 import json
 import datetime
 import random
-
 mininet_dir = "/home/pcc/mininet/"
-testing_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-data_dir = "/tmp/pcc_automated_testing/data/"
-results_base_dir = os.path.join(testing_dir, "results")
-#os.system("mkdir -p %s" % data_dir)
 sys.path.append(mininet_dir)
 from examples import sshd
 from mininet.link import TCLink
 from mininet.net import Mininet
 
-username = "pcc"
+data_dir = "/tmp/pcc_automated_testing/data/"
 
-"""
-def load_topology(topology_name):
-    topo_file = os.path.join(testing_dir, "topos", topology_name + ".json")
-    topo_json = None
-    with open(topo_file) as f:
-        topo_json = json.load(f)
-    return topo_json
-"""
+username = "pcc"
 
 def get_free_run_id():
     return random.randint(0, 2e9)
 
 def run_test(test_dict):
-    print(test_dict)
     test_name = "temp_test_name"
     date_string = datetime.date.today().strftime("%B_%d_%Y") + "_%d" % (int(round(time.time() * 1000)))
-    results_dir = os.path.join(results_base_dir, test["Name"], "%s_%s" % (test_name, date_string))
+    results_dir = os.path.join(file_locations.results_dir, test["Name"], "%s_%s" % (test_name, date_string))
     os.system("mkdir -p %s" % results_dir)
     test_topo_name = test_dict["Topology"]
     test_topo_dict = read_topology_to_dict(test_topo_name)
@@ -63,7 +51,6 @@ def run_test(test_dict):
         run_id = run_ids[i]
         convert_pantheon_log("%s/%s_datalink_run%d.log" % (data_dir, flow["protocol"], run_id),
             "%s/%s_%s.json" % (results_dir, flow["protocol"], flow["name"]))
-        #os.system("chmod 666 %s/%s_%d.json" % (results_dir, flow["protocol"], run_id))
 
     graphing_script = os.path.join(pantheon_setup.pan_dir, "..", "graphing", "pcc_grapher.py")
     graph_config = os.path.join(pantheon_setup.pan_dir, "..", "graphing", "graphs", "sample.json")
@@ -73,24 +60,9 @@ def run_test(test_dict):
 #   Load in the test descriptor files.
 ##
 
-test_file = os.path.join(testing_dir, "tests", sys.argv[2]) + ".json"
-test_json = None
-with open(test_file) as f:
-    test_json = json.load(f)
-print("Loaded test (list) from %s" % test_file)
-
-all_tests = []
-if "Tests" in test_json.keys():
-    test_filenames = test_json["Tests"]
-    for test_filename in test_filenames:
-        test_file = os.path.join(testing_dir, "tests", test_filename) + ".json"
-        test_json = None
-        with open(test_file) as f:
-            test_json = json.load(f)
-        print("Loaded test from %s" % test_file)
-        all_tests.append(test_json)
-else:
-    all_tests = [test_json]
+tests_to_run = sys.argv[2]
+all_test_names = read_test_list_to_list(tests_to_run)
+all_tests = [read_test_to_dict(t) for t in all_test_names]
 
 ##
 #   Begin executing tests. Note: Eventually tests should be grouped by topology so
@@ -98,7 +70,5 @@ else:
 #   better.
 ##
 
-print(all_tests)
 for test in all_tests:
-    print(test)
     run_test(test)
