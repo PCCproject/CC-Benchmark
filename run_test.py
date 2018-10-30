456b29f00993bb32fb103da18896c2b2d4735e26
Nathan Jay <njay2@illinois.edu>
2018-10-30 16:58:26 -0500

    Adding initial support for time-varying links.

--- run_test.py
+++ run_test.py
@@ -37,8 +37,10 @@ def run_test(test_dict):
     os.system("mkdir -p %s" % results_dir)
     test_topo_dict = read_topology_to_dict(test_dict["Topology"])
     test_link_types = test_dict["Link Types"]
-    net = Mininet(topo=sshd.MyTopo(test_topo_dict, test_link_types), link=TCLink)
+    topo = sshd.MyTopo(test_topo_dict, test_link_types)
+    net = Mininet(topo=topo, link=TCLink)
     sshd.sshd(net)
+    topo.start_all_link_managers(net)
     flows = test_dict["Flows"]
     run_ids = {}
     max_dur = 0
@@ -57,6 +59,7 @@ def run_test(test_dict):
         os.system("sudo -u %s ssh -i ~/.ssh/id_mininet_rsa %s \"%s\" &" % (username, flow["src"], test_command))
     sys.stderr.write("Sleeping for %d seconds" % (int(20 + max_dur)))
     time.sleep(30 + max_dur)
+    topo.stop_all_link_managers()
     net.stop()
     os.system("mn -c")
 
@@ -69,7 +72,7 @@ def run_test(test_dict):
     metadata = {"Scheme":scheme_to_test}
     with open(os.path.join(results_dir, "test_metadata.json"), "w") as f:
         json.dump(metadata, f)
-    os.system("rm -rf %s/*" % data_dir)
+    #os.system("rm -rf %s/*" % data_dir)
 
     graphing_script = os.path.join(file_locations.graphing_dir, "pcc_grapher.py")
     graph_config = os.path.join(file_locations.graphing_dir, "..", "graphing", "graphs", "sample.json")
