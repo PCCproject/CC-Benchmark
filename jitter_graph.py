c29c9fc0b6c366ddacde80d822fe137c5c8d098d
Nathan Jay <njay2@illinois.edu>
2019-04-12 13:30:41 -0500

    Updated jitter sweep graphing script.

--- jitter_graph.py
+++ jitter_graph.py
@@ -1,34 +1,62 @@
 #!/usr/bin/python3
+import sys
 import numpy as np
+import matplotlib as mpl
+mpl.use("Agg")
 import matplotlib.pyplot as plt
+mpl.style.use("fivethirtyeight")
 from graphing.analysis.results_library import ResultsLibrary, TestResult
 from graphing.utils import data_utils
 from python_utils.file_locations import results_dir
+from graphing.utils import basic_graphs
 
 results = ResultsLibrary(results_dir)
 
 param_name = "Jitter"
 param_unit = "ms from 30ms"
 params = [0, 1, 2, 3, 4, 5, 6, 8, 10]
-format_string = "jitter_30ms_%dms"
+format_string = "jitter_sweep.30ms_%dms"
 flow_name = "flow_1"
 
-full_schemes = results.get_all_schemes_with_tests([format_string % p for p in params])
-thpt_data = data_utils.get_stats_dict_from_param_test(results, full_schemes, flow_name, params,
-        format_string, "Throughput")
-lat_data = data_utils.get_stats_dict_from_param_test(results, full_schemes, flow_name, params,
-        format_string, "Avg Rtt")
+def thpt_transform(data):
+    return np.multiply(data, 0.001 * np.divide(1.0, 30))
 
-fig, axes = plt.subplots(2)
-thpt_axis = axes[0]
-lat_axis = axes[1]
-for scheme in full_schemes:
-    thpt_axis.plot(params, thpt_data[scheme], label=scheme)
-    lat_axis.plot(params, lat_data[scheme])
+def lat_transform(data):
+    return np.subtract(data, params)
 
-fig.legend()
-thpt_axis.set_title("%s Test Performance" % param_name)
-lat_axis.set_xlabel("%s (%s)" % (param_name, param_unit))
-thpt_axis.set_ylabel("Average Throughput (mbps)")
-lat_axis.set_ylabel("Average Latency (ms)")
-plt.show()
+def thpt_score(data):
+    return np.multiply(data, 0.1 * np.divide(1.0, 32.0))
+
+def lat_score(data):
+    return [100.0 - 100.0 * (d - 30) / (1500.0 * 8.0 / 32.0) for d in data]
+
+def loss_score(data):
+    return 100.0 * np.subtract(1.0, data)
+
+axes = [
+{"data": "Throughput",
+ "name": "Link Utilization",
+ "func": "plot",
+ "transform": thpt_transform},
+{"data": "Avg Rtt",
+ "name": "Self-inflicted Latency (ms)",
+ "func": "plot",
+ "transform": lat_transform}
+]
+
+schemes = None
+if len(sys.argv) > 1:
+    schemes = sys.argv[1].split(",")
+
+basic_graphs.make_sweep_table(format_string, params, flow_name, schemes, thpt_score, lat_score, loss_score)
+plt.savefig("jitter_scores.pdf", bbox_inches='tight')
+plt.cla()
+
+fig, axes = basic_graphs.make_sweep_graph(format_string, params, flow_name,
+        schemes, show_spread=True, axes=axes)
+
+fig.legend(loc='center', bbox_to_anchor=(0.65, 0.35))#loc='center right')
+axes[0].grid(True)
+axes[1].grid(True)
+axes[-1].set_xlabel("%s (%s)" % (param_name, param_unit))
+plt.savefig("jitter_sensitivity.pdf", bbox_inches='tight')
