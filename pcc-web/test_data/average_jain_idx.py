0193e4c1c47c3f9648b431e3cefc526aa611ed32
Jae Wook Lee <jlee759@illinois.edu>
2019-07-08 17:08:02 -0500

    aignore datafiles and add scripts to combine/average multiple test data

---
+++ pcc-web/test_data/average_jain_idx.py
@@ -0,0 +1,42 @@
+import json
+import os
+import sys
+import numpy as np
+
+def _add_jain_idx(scheme_list, to_add):
+    for elem in scheme_list:
+        if int(elem['x']) == int(to_add['x']):
+            elem['y'].append(to_add['y'])
+            return
+    scheme_list.append({'x':to_add['x'], 'y':[to_add['y']]})
+
+def _merge_jain_idx(scheme_dir):
+    scheme = scheme_dir.split('/')[-1]
+    res = []
+    for file in os.listdir(scheme_dir):
+        if not file.startswith('.DS') and not file.startswith('metric'):
+            file_path = scheme_dir + '/' + file
+            with open(file_path) as f:
+                obj = json.load(f)
+                x = obj['Ratio']
+                y = obj['Jain idx']
+                to_add = {'x':x, 'y':y}
+                _add_jain_idx(res, to_add)
+
+    for elem in res:
+        elem['y'] = np.mean(elem['y'])
+
+    return res
+
+def merge_jain_idx_in_dir(data_dir):
+    merged = {}
+    for scheme in os.listdir(data_dir):
+        if not scheme.startswith('.DS'):
+            merged[scheme] = _merge_jain_idx(data_dir + scheme)
+
+    with open(data_dir + '../jain.json', 'w') as f:
+        f.write(json.dumps(merged, indent=4))
+
+def merge_all_jain_idx_with_multiflow(test_data_dir, multiflow_dir):
+    for mult_dir in multiflow_dir:
+        merge_jain_idx_in_dir(test_data_dir + mult_dir + '/data/')
---
+++ pcc-web/test_data/average_metric.py
@@ -0,0 +1,89 @@
+import json
+import os
+import sys
+import numpy as np
+from pprint import pprint
+
+def _average_single_merged_metric(merged_metric):
+    sum = set()
+    sum.add('Link Util')
+    # add more if necessary
+
+    for k in merged_metric.keys():
+        if k in sum:
+            merged_metric[k] = np.sum(merged_metric[k])
+        else:
+            merged_metric[k] = np.mean(merged_metric[k])
+    return merged_metric
+
+def _merge_single_trial(metric):
+    merged_metric = {}
+    for flow, detail in metric.items():
+        for field, value in metric[flow].items():
+            # print(field)
+            if not field in merged_metric:
+                merged_metric[field] = []
+            merged_metric[field].append(value)
+
+    return _average_single_merged_metric(merged_metric)
+
+def _merge_multiple_test(metrics_list):
+    if len(metrics_list) == 1:
+        return metrics_list[0]
+
+    merged_metric = {}
+    for metric in metrics_list:
+        for k, v in metric.items():
+            if k not in merged_metric:
+                merged_metric[k] = []
+            merged_metric[k].append(v)
+    # print(merged_metric)
+
+    for k, v in merged_metric.items():
+        merged_metric[k] = np.mean(merged_metric[k])
+
+    return merged_metric
+
+def average_all_trials(metrics):
+    # pprint(metrics)
+    for scheme in metrics.keys():
+        # print(scheme)
+        for detail, metric in metrics[scheme].items():
+            metrics[scheme][detail] = _merge_multiple_test(metrics[scheme][detail])
+
+    return metrics
+
+def merge_metric_for_all_test(test_data_dir, multiflow_dir):
+    for test_dir in os.listdir(test_data_dir):
+        test_path = test_data_dir + test_dir
+        if os.path.isdir(test_path) and 'pycache' not in test_dir:
+            data_path = test_path + '/data/'
+            res = {}
+            for scheme in os.listdir(data_path):
+                scheme_path = data_path + scheme
+                if os.path.isdir(scheme_path):
+                    res[scheme] = {}
+                    for file in os.listdir(scheme_path):
+                        filepath = scheme_path + '/' + file
+                        if file.startswith('metric'):
+                            # print(scheme)
+                            # print(filepath)
+                            with open(filepath) as f:
+                                metric = json.load(f)
+
+                            if len(metric[scheme]) > 1:
+                                multiflow_dir.add(test_dir)
+                                metric = _merge_single_trial(metric[scheme])
+                            else:
+                                metric = metric[scheme]['flow1']
+
+                            test_detail = str(file.split('-')[1])
+                            if test_detail not in res[scheme]:
+                                res[scheme][test_detail] = []
+                            res[scheme][test_detail].append(metric)
+            if len(res) > 0:
+                # pprint(res)
+                res = average_all_trials(res)
+                metric_file_name = test_path + '/metrics.json'
+                with open(metric_file_name, 'w') as f:
+                    f.write(json.dumps(res, indent=4))
