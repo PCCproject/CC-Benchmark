bfb89cb6fded2c570251cd4cb640ae6e1b19314a
Jae Wook Lee <leejwook0429@gmail.com>
2019-07-11 19:33:10 -0500

    fix rsync script

--- pcc-web/test_data/rsync_with_cpanel.py
+++ pcc-web/test_data/rsync_with_cpanel.py
@@ -4,11 +4,10 @@ import os
 # Current host is ocean0
 
 rsa_path = '/srv/shared/PCC/.web_rsa/web_rsa'
-testdata_path = '/srv/shared/PCC/testing/pcc-web/test_data/*'
-website_path = 'pcctesting@web.illinois.edu:/home/pcctesting/public_html/test_data'
+testdata_path = '/srv/shared/PCC/testing/pcc-web/*'
+website_path = 'pcctesting@web.illinois.edu:/home/pcctesting/public_html/'
 
 def rsync_with_cpanel():
     cmd = 'rsync -r -avz -e "ssh -i {}" {} {}'.format(rsa_path, testdata_path, website_path)
-    print("Executing {}".format(cmd))
     print("Updating results to web...")
     os.system(cmd)
