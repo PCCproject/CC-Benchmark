faca85abfdb42923a14c7ef68237b709604511b8
Jae Wook Lee <jlee759@illinois.edu>
2019-07-12 21:26:12 -0500

    change path wrt to ocean0

--- pcc-web/test_data/update.py
+++ pcc-web/test_data/update.py
@@ -6,12 +6,12 @@ import rsync_with_cpanel
 import get_overall_score
 
 def main():
-    test_data_dir = '/Users/jaewooklee/PCC-Tester/pcc-web/test_data/'
+    test_data_dir = '/srv/shared/PCC/testing/pcc-web/test_data/'
     multiflow_dir = set()
     average_metric.merge_metric_for_all_test(test_data_dir, multiflow_dir)
     average_jain_idx.merge_all_jain_idx_with_multiflow(test_data_dir, multiflow_dir)
     get_overall_score.get_overall_score(test_data_dir)
-    # rsync_with_cpanel.rsync_with_cpanel()
+    rsync_with_cpanel.rsync_with_cpanel()
 
 if __name__ == '__main__':
     main()
