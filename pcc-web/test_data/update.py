4cbbaca89f6e418a023ecd9b3a83aa104cee22ca
Jae Wook Lee <jlee759@ocean0.cs.illinois.edu>
2019-07-22 19:13:09 -0500

    grab file location automatically

--- pcc-web/test_data/update.py
+++ pcc-web/test_data/update.py
@@ -4,14 +4,15 @@ import average_metric
 import average_jain_idx
 import rsync_with_cpanel
 import get_overall_score
+import os
 
 def main():
-    test_data_dir = '/Users/jaewooklee/PCC-Tester/pcc-web/test_data/'
+    test_data_dir = os.path.dirname(os.path.abspath(__file__)) + '/' 
     multiflow_dir = set()
     average_metric.merge_metric_for_all_test(test_data_dir, multiflow_dir)
     average_jain_idx.merge_all_jain_idx_with_multiflow(test_data_dir, multiflow_dir)
     get_overall_score.get_overall_score(test_data_dir)
-    # rsync_with_cpanel.rsync_with_cpanel()
+    rsync_with_cpanel.rsync_with_cpanel()
 
 if __name__ == '__main__':
     main()
