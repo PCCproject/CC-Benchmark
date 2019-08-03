9c9df7d833b92190371496dcd22352859597e7d7
Jae Wook Lee <jlee759@illinois.edu>
2019-08-02 22:47:53 -0500

    automatically populate schemes

--- pcc-web/js/fill_table.js
+++ pcc-web/js/fill_table.js
@@ -1,8 +1,41 @@
 var json_data = undefined;
+var indev_data = undefined;
+var listOfIndevScheme = new Set();
 
 $.getJSON('../test_data/public_scheme_metrics.json', function(data) {
   json_data = data;
 });
+
+$.getJSON('../test_data/indev_scheme_metrics.json', function(data) {
+  indev_data = data;
+  for (var i = 0; i < indev_data.Tests.length; i++) {
+    var curr_test = indev_data.Tests[i].overall;
+    Object.keys(curr_test).forEach(item => {
+      var c = item[item.length - 6];
+      if (c != ' ') {
+        listOfIndevScheme.add(item);
+      }
+    });
+  }
+});
+
+function populateIndevScheme() {
+  var tableHead = "<th class='description' style='text-decoration: none;'>Test Scenario</th>";
+  var html_bracket = '<th class="cell" style="white-space:pre">';
+  var html_bracket_end = '</th>';
+
+  listOfIndevScheme.forEach(item => {
+    tableHead += html_bracket;
+    var split = item.split(',');
+    tableHead += split[0];
+    tableHead += ':<br>';
+    tableHead += split[1];
+    tableHead += ':<br>';
+    tableHead += split[2];
+    tableHead += html_bracket_end;
+  });
+  document.getElementById('indev-scheme').innerHTML = tableHead;
+}
 
 function get_rgb(normalized_val) {
   if (normalized_val === "N/A" || normalized_val == undefined) {
@@ -81,9 +114,7 @@ function table_formatter(type) {
     var vegas_bg = get_rgb(metrics["vegas score"]);
     var sprout_bg = get_rgb(metrics["sprout score"]);
     var ledbat_bg = get_rgb(metrics["ledbat score"]);
-    // console.log(vivace_bg)
-    // console.log(vivace)
-    //background-color: rgb(201, 76, 76);
+   
     var displayName = getDisplayName(curr_test);
 
     return_str += ("<th class='description'><a href=test_data/" + test_name + "/index.html>" + displayName + "</a></th>");
@@ -95,6 +126,30 @@ function table_formatter(type) {
     return_str += ("<th class='cell' style='" + vegas_bg + "'>" + vegas + "</th>");
     return_str += ("<th class='cell' style='" + sprout_bg + "'>" + sprout + "</th>");
     return_str += ("<th class='cell' style='" + ledbat_bg + "'>" + ledbat + "</th>");
+    return_str += "</tr>";
+  }
+
+  return return_str;
+}
+
+function indev_table_formatter(type) {
+  var return_str = "";
+  for (var i = 0; i < indev_data.Tests.length; i++) {
+    return_str += "<tr>";
+    var curr_test = indev_data.Tests[i];
+    var test_name = curr_test.name;
+    var metrics = curr_test[type];
+    var displayName = getDisplayName(curr_test);
+    console.log(metrics);
+    return_str += ("<th class='description'><a href=test_data/" + test_name + "/index.html>" + displayName + "</a></th>");
+
+    listOfIndevScheme.forEach(item => {
+      console.log(item);
+      var raw_score = (metrics[item] == undefined) ? "N/A" : round(metrics[item]);
+      var bg_color = get_rgb(metrics[item+' score']);
+      return_str += ("<th class='cell' style='" + bg_color + "'>" + raw_score + "</th>");
+    });
+
     return_str += "</tr>";
   }
 
@@ -128,6 +183,38 @@ function disp_thpt() {
 function disp_loss() {
   // console.log(json_data);
   var s = table_formatter('avg loss');
+  document.getElementById("table-body").innerHTML = s;
+  document.getElementById("metric-description").innerHTML = '<pre>Number: Mean fraction of packets lost' +
+  '<br>Color: 1 - 10*( (measured loss - random loss) / (1 - random loss) )';
+}
+
+function disp_indev_power_metric() {
+  // console.log(json_data);
+  var s = indev_table_formatter('overall');
+  document.getElementById("table-body").innerHTML = s;
+  document.getElementById("metric-description").innerHTML = '<pre>Number: log(mean throughput / 95% delay) (Kleinrock\'s power metric)' +
+  '<br>Color: log(mean throughput / 95% delay) / 5';
+}
+
+function disp_indev_latency() {
+  // console.log(json_data);
+  var s = indev_table_formatter('95 qdelay');
+  document.getElementById("table-body").innerHTML = s;
+  document.getElementById("metric-description").innerHTML = '<pre>Number: 95% queueing delay among all tests, in ms' +
+  '<br>Color: 1 - (95% delay / 60ms)';
+}
+
+function disp_indev_thpt() {
+  // console.log(json_data);
+  var s = indev_table_formatter('avg thrput');
+  document.getElementById("table-body").innerHTML = s;
+  document.getElementById("metric-description").innerHTML = '<pre>Number: Mean throughput among all tests, in Mbps' +
+  '<br>Color: Mean throughput / link bottleneck';
+}
+
+function disp_indev_loss() {
+  // console.log(json_data);
+  var s = indev_table_formatter('avg loss');
   document.getElementById("table-body").innerHTML = s;
   document.getElementById("metric-description").innerHTML = '<pre>Number: Mean fraction of packets lost' +
   '<br>Color: 1 - 10*( (measured loss - random loss) / (1 - random loss) )';
